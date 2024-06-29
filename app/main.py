ee7cc0653be990006b84eb43102acc7ea72043e1
Asurasuu <andrey.novoselov.v@yandex.ru>
2024-06-29 20:18:48 +0600

    Натянул верстку на роуты

--- app/main.py
+++ app/main.py
@@ -1,15 +1,54 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+app.mount("/images", StaticFiles(directory="./images"), name="images")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="./pages")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Дальше роуты
+
+# Главная страница
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    template = "index.html"
+    context = {"request": request}
+
+    return templates.TemplateResponse(template, context)
+
+# Страница сообщений
+@app.get("/messanger", response_class=HTMLResponse)
+async def read_item(request: Request):
+    template = "messanger.html"
+    context = {"request": request}
+
+    return templates.TemplateResponse(template, context)
+
+# Страница о проекте
+@app.get("/about", response_class=HTMLResponse)
+async def read_item(request: Request):
+    template = "about.html"
+    context = {"request": request}
+
+    return templates.TemplateResponse(template, context)
+
+# Страница профиль
+@app.get("/profile", response_class=HTMLResponse)
+async def read_item(request: Request):
+    template = "profile.html"
+    context = {"request": request}
+
+    return templates.TemplateResponse(template, context)
+
+# Страница настройки
+@app.get("/settings", response_class=HTMLResponse)
+async def read_item(request: Request):
+    template = "settings.html"
+    context = {"request": request}
+
+    return templates.TemplateResponse(template, context)
